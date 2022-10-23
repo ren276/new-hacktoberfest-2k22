8577fa47d4754cb34ffe6b2009d0667061ffe1ec
Sandesh Verma <sandeshv414@gmail.com>
2022-10-23 12:18:09 +0530

    update

--- Spotify clone/app/src/main/java/com/shaun/spotonmusic/SpotOnApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.shaun.spotonmusic
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SpotOnApplication:Application() {
-}
--- Spotify clone/app/src/main/java/com/shaun/spotonmusic/database/model/SpotOnMusicModel.kt
+++
@@ -1,8 +0,0 @@
-package com.shaun.spotonmusic.database.model
-
-data class SpotOnMusicModel(
-    var imageUrls: List<String>,
-    var type: String,
-    var title: String,
-    val id: String
-)
